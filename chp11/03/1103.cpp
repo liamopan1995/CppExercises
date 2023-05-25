138e6adcd0bd3cf4c6ea70e2b9d693b5802c366a
Liam <huxinpanchina.com>
2023-05-25 12:35:37 +0200

    string to number accomplished , negative case need to be target at next

--- chp11/03/1103.cpp
+++ chp11/03/1103.cpp
@@ -19,8 +19,19 @@ int main () {
     input = "3A";
     
     cout << BinaryToDecimal(input,11)<<endl;
+    input = "3B";
+    
+    cout << BinaryToDecimal(input,11)<<endl;
+    cout << BinaryToDecimal(input,12)<<endl;
     input = "3K";
     cout << BinaryToDecimal(input,11)<<endl;
+
+    input = "3&";
+    cout << BinaryToDecimal(input,11)<<endl;
+
+    input = "XX";
+    cout << BinaryToDecimal(input,36)<<endl;
+
     return 0;
 }
 
@@ -46,13 +57,30 @@ int BinaryToDecimal(string &str, int base){
     // }
 
     // validation check
+    char max = '0';
     for (int i = 0;  i < str.length(); i++ ) {
-        if ( !('0' <= str[i] && str[i] <= '9' || 'A' <= str[i] && str[i] <= 'B')) {
-            cout<< "digits are outof range for base: "<<base<<endl;
+        if ( !('0' <= str[i] && str[i] <= '9' || 'A' <= str[i] && str[i] <= 'Z')) {
             cout <<"invalid digit"<<"  "<< str[i]<<endl;
             return -1;
         }
+
+        max = (str[i] > max)? str[i]:max;
+
+        if ('0'<=max && max <='9' ) {
+           if (max - '0'>= base) {
+            cout << " digit out of range  " << max<<endl ;
+            return -1;
+           }
+ 
+        } 
+        else {
+            if (max -'7'>= base ) {
+            cout << " digit out of range  " << max<<endl ;
+            return -1;
+           }
+        }
     }
+
 
 
 
@@ -74,6 +102,30 @@ int BinaryToDecimal(string &str, int base){
             case '9': result +=  9 * pow(base,exponent); break;
             case 'A': result +=  10 * pow(base,exponent); break;
             case 'B': result +=  11 * pow(base,exponent); break;
+            case 'C': result +=  12 * pow(base,exponent); break;
+            case 'D': result +=  13 * pow(base,exponent); break;
+            case 'E': result +=  14 * pow(base,exponent); break;
+            case 'F': result +=  15 * pow(base,exponent); break;
+            case 'G': result +=  16 * pow(base,exponent); break;
+            case 'H': result +=  17 * pow(base,exponent); break;
+            case 'I': result +=  18 * pow(base,exponent); break;
+            case 'J': result +=  19 * pow(base,exponent); break;
+            case 'K': result +=  20 * pow(base,exponent); break;
+            case 'L': result +=  21 * pow(base,exponent); break;
+            case 'M': result +=  22 * pow(base,exponent); break;
+            case 'N': result +=  23 * pow(base,exponent); break;
+            case 'O': result +=  24 * pow(base,exponent); break;
+            case 'P': result +=  25 * pow(base,exponent); break;
+            case 'Q': result +=  26 * pow(base,exponent); break;
+            case 'R': result +=  27 * pow(base,exponent); break;
+            case 'S': result +=  28 * pow(base,exponent); break;
+            case 'T': result +=  29 * pow(base,exponent); break;
+            case 'U': result +=  30 * pow(base,exponent); break;
+            case 'V': result +=  31 * pow(base,exponent); break;
+            case 'W': result +=  32 * pow(base,exponent); break;
+            case 'X': result +=  33 * pow(base,exponent); break;
+            case 'Y': result +=  34 * pow(base,exponent); break;
+            case 'Z': result +=  35 * pow(base,exponent); break;
             
 
 
