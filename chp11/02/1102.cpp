6c02c277e66127df99bc2176ca0be50e6f62e0ca
Liam <huxinpanchina.com>
2023-05-25 10:18:24 +0200

    finished all the work ,code runs perfectly ,I got one question with pointer though

--- chp11/02/1102.cpp
+++ chp11/02/1102.cpp
@@ -2,8 +2,13 @@
 #include <math.h>
 using namespace std;
 
+
+// functin prototypes
 void generateGrayCode(int nBits);
 void generateGrayCode_core(int nBits,string* arr, int arrLen);
+
+
+
 int main() {
 
     int a;
@@ -13,35 +18,44 @@ int main() {
     return 0;
 }
 
+
+// declaration of wrapper function
 void generateGrayCode(int nBits){
     if (nBits < 1) {
          cout<<"invalid nBits!"<<endl;
     }
     else {
-        string* strArray = new string[2]{"02","19"};
+        string* strArray = new string[2]{"0","1"};
         generateGrayCode_core(nBits,strArray,2);
         delete[] strArray;  // Deallocate the dynamically allocated array
     }
 
 }
-
+// Declaration of recursive function
 void generateGrayCode_core(int nBits,string* arr, int arrLen) {
+    string *ptr1 = arr;
     if (nBits == 1) {
         for (int i = 0; i < arrLen; i++){
             if(arr != nullptr) {
                 // string* ptr_template= arr;
-                cout << *arr++ << endl;
-                
-        } 
-        // delete[] arr;
+                cout << *arr++ <<"  -> "<< i<< endl;            
+            } 
         }
         
     }
 
     else {
-        return;
+        string* strArray = new string[2* arrLen];
+        
+        for (int i = 0; i < arrLen; i++) {
+            strArray[i] = "0" + *arr++;
+        }
+        for (int i = 2*arrLen -1; i >= arrLen; i--) {
+            strArray[i] = "1" + *ptr1++;  // why is not allowed to continue use pointer arr at here?
+        }
+
+        generateGrayCode_core(nBits-1, strArray, 2*arrLen);
+        delete[] strArray;
     }
 
 }
-
-// void display 
