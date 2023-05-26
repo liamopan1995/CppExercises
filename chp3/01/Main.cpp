1af5c6ecd3b21237b633412eee47b1a51cdc4416
Liam <huxinpanchina.com>
2023-05-26 10:51:08 +0200

    dealt with library dependence

--- chp3/01/Main.cpp
+++ chp3/01/Main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "string"
-#include "strlib.h"
 
 using namespace std;
 
@@ -13,16 +12,16 @@ using namespace std;
 bool endsupWith(string str,string suffix);
 // main
 int main() {
-    cout<<"we expeting the answer will be 0 on this "<<"cwcdick "<<" mm"<<endl;
-    bool token = endsWith("cwcdick","mm" );
-    cout<< token <<endl;
-    cout<<"we expeting the answer will be 1 on this "<<"cwcdick "<<" dick"<<endl;
-    token = endsWith("cwcdick","dick" );
-    cout<< token <<endl;
-    cout<<"we expeting the answer will be 1 on this "<<"cwcDICK "<<" dick"<<endl;
-    token = endsWith("cwcdick","dick" );  /*thankfully this works so the work to
+    cout<<"we expeting the answer should be 0 on this str "<<"cwcdick "<<"and suffix: "<<" mm"<<endl;
+    bool token = endsupWith("cwcdick","mm" );
+    cout<< "The anwser is:" << " "<<token <<endl;
+    cout<<"we expeting the answer should be 1 on this str "<<"cwcdick "<<"and suffix: "<<" dick"<<endl;
+    token = endsupWith("cwcdick","dick" );
+    cout<< "The anwser is:" << " "<<token <<endl;
+    cout<<"we expeting the answer should be 1 on this str "<<"cwcDICK "<<"and suffix: "<<" dick"<<endl;
+    token = endsupWith("cwcdick","dick" );  /*thankfully this works so the work to
 upercase them saved */
-    cout<< token <<endl;
+    cout<< "The anwser is:" << " "<<token <<endl;
     return 0;
 }
 // declaration
